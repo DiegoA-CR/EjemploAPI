7f0bba4a08ba0408e10b5b7e3fa4dfd7e2a38f30
DiegoA-CR <diego.a.calvarior24@gmail.com>
2026-07-30 20:42:30 -0600

    corregir la creacion del array en el PUT

--- Ejempl_Back_Python/app.py
+++ Ejempl_Back_Python/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify;
+from flask import Flask, jsonify, request;
 from productos import productos;
 
 app = Flask(__name__);
@@ -26,11 +26,34 @@ def getDos(producto_nombre):
     productoEncontrado = [producto for producto in productos if producto['Nombre'] == producto_nombre]
     if(len(productoEncontrado)> 0):
         return jsonify({"Producto": productoEncontrado[0]})
-
+    return jsonify({"message": "The product no exist"})
+#actualizar lista de productos
 @app.route('/productos', methods = ['POST'])
-def addProducto():
-    return 'resibido'
+def agregaProducto():
+ #print(request.json) verificar que si esta recibiendo datos json
+ #return 'recibido'
+    new_product = {
+        "Nombre": request.json["Nombre"],
+        "Precio": request.json["Precio"],
+        "Cantidad": request.json["Cantidad"],
+    }
+    productos.append(new_product)
+    return jsonify({"message": "El producto fue agragado exitosamente", "productos": productos})
+# editar valores de objeto 
+@app.route('/productos/<string:producto_nombre>', methods = ['PUT'])
+def editarProducto(producto_nombre):
+    buscaProducto = [producto for producto in productos if producto['Nombre'] == producto_nombre]
+    if (len(buscaProducto) > 0):
+        buscaProducto[0]["Nombre"] = request.json["Nombre"],
+        buscaProducto[0]["Precio"] = request.json["Precio"],
+        buscaProducto[0]["Cantidad"] = request.json["Cantidad"]
+        return jsonify({
+        "message": "Producto Actualizado",
+        "producto": buscaProducto[0]
+        })
+    return jsonify({"message":"No se encuentra el producto"})
 
+#----------------------------------------------
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
 
