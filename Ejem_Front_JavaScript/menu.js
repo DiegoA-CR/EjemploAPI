b8fc81183d8b67927d6a9f9da0b33f121c94dcde
DiegoA-CR <diego.a.calvarior24@gmail.com>
2026-08-01 17:03:21 -0600

    01082026

--- Ejem_Front_JavaScript/menu.js
+++ Ejem_Front_JavaScript/menu.js
@@ -1,68 +1,75 @@
 //ejemplo API_Rest
 const apiURL = "https://rickandmortyapi.com/api/character"; // llamada a la API
 
-function creaCard(character){
-    const cardsContainer = document.querySelector('#cards-container');
-    const {name, status, image } = character;
-    //titulo
+function creaCard(character) {
+  const cardsContainer = document.querySelector("#cards-container");
+  const { name, status, image, species, gender } = character;
 
-    const title = document.createElement("h4");
-    title.textContent = name;
+  //titulo
 
-    // caracteristicas
+  const nombre = document.createElement("h2");
+  nombre.textContent = name;
 
-    const characterStatus = document.createElement('p');
-    characterStatus.textContent = status;
+  const especie = document.createElement("h4");
+  especie.textContent = species;
 
-    if(status == "Alive"){
-        
-        characterStatus.style.color = "Green";
+  if (species != "Human") {
+    especie.className = "otra_especie";
+    especie.style.color = "red";
+  }
 
-    }else{
-        characterStatus.style.color = "Red";
-    }
-    
-    //imagenes
+  const genero = document.createElement("h5");
+  genero.textContent = gender;
+  genero.className = "genero_personaje";
+  // caracteristicas
 
-    const imageCharacter = document.createElement('img');
-    imageCharacter.src = image;
-    imageCharacter.width = 300;
+  const characterStatus = document.createElement("p");
+  characterStatus.textContent = status;
 
-    const Card = document.createElement('div');// creamos el contenedor
-    Card.appendChild(title);// Le agrgamos el titulo
-    Card.appendChild(imageCharacter); // la imagen
-    Card.appendChild(characterStatus);// y el estado
-    Card.style.backgroundColor = "Grey";
+  if (status == "Alive") {
+    characterStatus.className = "vivo";
+  } else {
+    characterStatus.className = "muerto";
+  }
 
-    //unimos todo
-    cardsContainer.appendChild(Card);
+  //imagenes
 
-    console.log(cardsContainer);
+  const imageCharacter = document.createElement("img");
+  imageCharacter.src = image;
+  imageCharacter.width = 300;
+
+  const Card = document.createElement("div"); // creamos el contenedor
+  Card.appendChild(nombre); // Le agrgamos el titulo
+  Card.appendChild(imageCharacter); // la imagen
+  Card.appendChild(especie);
+  Card.appendChild(genero);
+  Card.appendChild(characterStatus); // y el estado
+  Card.className = "card_stylie";
+
+  //unimos todo
+  cardsContainer.appendChild(Card);
+
+  //console.log(cardsContainer);
 }
 
-// funcion asincrona 
+// funcion asincrona
 
-async function getCaracters(){
-    
-    //intenta
-    try{
-        //consume api
-        const response = await fetch(apiURL);
-        //espera json
-        const {results} = await response.json();
-        
-        console.log(results);
+async function getCaracters() {
+  //intenta
+  try {
+    //consume api
+    const response = await fetch(apiURL);
+    //espera json
+    const { results } = await response.json();
 
-        for(let i=0; i<results.length;i++){
-            creaCard(results[i]);
-        }
+    console.log(results);
 
-    }catch(error){
-
-        console.error(error);
-
+    for (let i = 0; i < results.length; i++) {
+      creaCard(results[i]);
     }
+  } catch (error) {
+    console.error(error);
+  }
 }
-
 
 getCaracters();
